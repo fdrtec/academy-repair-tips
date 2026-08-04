aff697a71fdfc42d7b23ae2b8f2cb919a2f6e246
fdrtec <fdrtec@gmail.com>
2026-08-03 23:15:04 -0300

    FDR01 | feat: Initialize Repair Tips API project structure with main application class, properties, and test cases

---
+++ src/main/java/br/com/fdrtec/repair_tips_api/RepairTipsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fdrtec.repair_tips_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairTipsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairTipsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fdrtec/repair_tips_api/RepairTipsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fdrtec.repair_tips_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairTipsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
